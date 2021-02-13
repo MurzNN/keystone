e782a5aae0ab75f083f3c972c806e57bb536f1fe
Alexey Murz Korepov <MurzNN@gmail.com>
2021-02-13 14:39:31 +0300

    Allow id value on GraphQL create mutations inputs

--- packages/keystone/lib/ListTypes/list.js
+++ packages/keystone/lib/ListTypes/list.js
@@ -255,9 +255,12 @@ module.exports = class List {
   }
 
   getFieldsWithAccess({ schemaName, access }) {
-    return this.fields
-      .filter(({ path }) => path !== 'id') // Exclude the id fields update types
+    let fieldsWithAccess = this.fields
       .filter(field => field.access[schemaName][access]); // If it's globally set to false, makes sense to never let it be updated
+    if (access != 'create') {
+      fieldsWithAccess = fieldsWithAccess.filter(({ path }) => path !== 'id') // Exclude the id fields for non-create operations
+    }
+    return fieldsWithAccess;
   }
 
   getAllFieldsWithAccess({ schemaName, access }) {
