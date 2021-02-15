21a33e61147e009b58c98a4732f3bc89f11b44fb
Alexey Murz Korepov <MurzNN@gmail.com>
2021-02-15 15:21:29 +0300

    Add support for id in adapter-mongoose

--- packages/adapter-mongoose/lib/adapter-mongoose.js
+++ packages/adapter-mongoose/lib/adapter-mongoose.js
@@ -299,6 +299,11 @@ class MongooseListAdapter extends BaseListAdapter {
   }
 
   async _createSingle(realData) {
+    // Converting passed id value to Mongo's _id field
+    if (realData.id) {
+      realData._id = realData.id;
+      delete realData.id;
+    }
     const item = (await this.model.create(realData)).toObject();
 
     const itemId = item._id;
